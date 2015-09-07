3fe217cab5c7fa66c9b3d141d03bd8b9ae8b5964
unknown <kirito_baz@outlook.com>
2015-09-07 11:11:22 -0500

    Se agregaron las pruebas para todas las opciones disponibles

--- Edades.py
+++ Edades.py
@@ -1,20 +1,35 @@
-# -*- coding:utf-8 -*-
+﻿# -*- coding:utf-8 -*-
 
 def edades(edad):
     """
-    >>> edades(1)
+    >>> edades(-1)
     No existes
+
+    >>> edades(12)
+    Eres un niño
+
+    >>> edades(17)
+    Eres adolescente
+    
+    >>> edades(64)
+    Eres adulto
+
+    >>> edades(119)
+    Adulto mayor
+
+    >>> edades(123)
+    Eres Mumm-Ra
 
     """
     if edad < 0:
         print ("No existes")
-    elif edad < 13:
+    elif edad <= 13:
         print ("Eres un niño")
-    elif edad < 18:
+    elif edad <= 18:
         print ("Eres adolescente")
-    elif edad <65:
+    elif edad <= 65:
         print ("Eres adulto")
-    elif edad < 120:
+    elif edad <= 120:
         print ("Adulto mayor")
     else:
     	print ("Eres Mumm-Ra")
